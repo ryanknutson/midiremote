0790aadeca1ef9a218bc6513100aa99a782edc54
Ryan Knutson <ryanknut@me.com>
2017-12-12 00:45:57 +0000

    done with ajax

--- server.js
+++ server.js
@@ -1,84 +1,69 @@
-//
-// # SimpleServer
-//
-// A simple chat server using Socket.IO, Express, and Async.
-//
-var http = require('http');
-var path = require('path');
-
-var async = require('async');
-var socketio = require('socket.io');
 var express = require('express');
+var fs = require('fs');
+var path = require("path");
+var app = express();
 
-//
-// ## SimpleServer `SimpleServer(obj)`
-//
-// Creates a new instance of SimpleServer with the following options:
-//  * `port` - The HTTP port to listen on. If `process.env.PORT` is set, _it overrides this value_.
-//
-var router = express();
-var server = http.createServer(router);
-var io = socketio.listen(server);
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "views"));
 
-router.use(express.static(path.resolve(__dirname, 'client')));
-var messages = [];
-var sockets = [];
+// Port the server runs on
+var port = 8080;
 
-io.on('connection', function (socket) {
-    messages.forEach(function (data) {
-      socket.emit('message', data);
-    });
 
-    sockets.push(socket);
 
-    socket.on('disconnect', function () {
-      sockets.splice(sockets.indexOf(socket), 1);
-      updateRoster();
-    });
+app.use(express.bodyParser());
 
-    socket.on('message', function (msg) {
-      var text = String(msg || '');
-
-      if (!text)
-        return;
-
-      socket.get('name', function (err, name) {
-        var data = {
-          name: name,
-          text: text
-        };
-
-        broadcast('message', data);
-        messages.push(data);
-      });
-    });
-
-    socket.on('identify', function (name) {
-      socket.set('name', String(name || 'Anonymous'), function (err) {
-        updateRoster();
-      });
-    });
-  });
-
-function updateRoster() {
-  async.map(
-    sockets,
-    function (socket, callback) {
-      socket.get('name', callback);
-    },
-    function (err, names) {
-      broadcast('roster', names);
-    }
-  );
-}
-
-function broadcast(event, data) {
-  sockets.forEach(function (socket) {
-    socket.emit(event, data);
-  });
-}
-
-server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
-  var addr = server.address();
-  console.log("Chat server listening at", addr.address + ":" + addr.port);
+app.post('/search', function(req, res){
+	getTitles(req.body.search, res);
 });
+
+app.post('/play', function(req, res){
+	console.log('playing...', req.body.file);
+	res.send(200,{"Content-Type": "application/json"});
+});
+
+app.get('/', function (req, res) {
+  res.render("index");
+});
+
+
+
+function getTitles(search, res) {
+    
+    var regex = reg(search);
+    var results = [];
+    
+    fs.readFile("midilist.txt", "utf8", function(err, data) {
+        if (err) throw err;
+        
+        // set a variable to make things shorter
+        let m;
+        
+        // loop through the data and add them to the array
+        while ((m = regex.exec(data)) !== null) {
+            if (m.index === regex.lastIndex) {
+                regex.lastIndex++;
+            }
+            
+            m.forEach((match, groupIndex) => {
+                results.push(match);
+                
+            });
+        }
+        
+        // send results to client
+        res.json(results);
+    });
+}
+
+
+function reg(input) {
+    var flags;
+    //could be any combination of 'g', 'i', and 'm'
+    flags = 'gi';
+    return new RegExp('.*' + input + '.*', flags);
+}
+
+
+console.log("Started on port", port);
+app.listen(port);
